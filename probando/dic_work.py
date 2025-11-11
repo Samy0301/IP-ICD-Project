eb8edb9e2632a79fad94e6b8717bb319c46b34fb
Samy <samyackerman0@gmail.com>
2025-11-11 16:36:32 -0500

    comparacion de promedio de precios por producto

--- probando/dic_work.py
+++ probando/dic_work.py
@@ -5,17 +5,67 @@ salario_medio = 5922.60
 with open("C:\\Users\\pc\\Desktop\\CD\\repos\\IP-ICD-Project\\probando\\prueba.json", "r", encoding="utf-8") as f:
     jason=json.load(f)
 
-lst_promedio_todas=[]
+lst_p1=[]
+lst_p2=[]
+lst_p3=[]
+lst_p4=[]
+lst_p5=[]
+lst_p6=[]
+lst_p7=[]
+lst_p8=[]
+lst_p9=[]
+lst_p10=[]
 for item in jason:
-    for i in item.keys():
-        if i == "products":
-            lst=[x for x in item[i].values()]
-            lst_promedio_todas.append(metodos_pruebas.promedio(lst)) 
+    lst_p1.append(item["products"]["1"])
+    lst_p2.append(item["products"]["2"])
+    lst_p3.append(item["products"]["3"])
+    lst_p4.append(item["products"]["4"])
+    lst_p5.append(item["products"]["5"])
+    lst_p6.append(item["products"]["6"])
+    lst_p7.append(item["products"]["7"])
+    lst_p8.append(item["products"]["8"])
+    lst_p9.append(item["products"]["9"])
+    lst_p10.append(item["products"]["10"])
 
-promedio_offline=metodos_pruebas.promedio(lst_promedio_todas)
+lst=[lst_p1, lst_p2, lst_p3, lst_p4, lst_p5, lst_p6, lst_p7, lst_p8, lst_p9, lst_p10]
+print(lst)
+lst_promedio_off=[]
+for i in lst:
+    lst_promedio_off.append(metodos_pruebas.promedio(i))
 
-num, text = metodos_pruebas.porciento_salario_gastado(promedio_offline, salario_medio)
-print(f"{num}% {text}")
+
+with open("C:\\Users\\pc\\Desktop\\CD\\repos\\IP-ICD-Project\\probando\\prueba_online.json", "r", encoding="utf-8") as f:
+    jason1=json.load(f)
+
+l_p1=[]
+l_p2=[]
+l_p3=[]
+l_p4=[]
+l_p5=[]
+l_p6=[]
+l_p7=[]
+l_p8=[]
+l_p9=[]
+l_p10=[]
+for item in jason1:
+    l_p1.append(item["products"]["1"])
+    l_p2.append(item["products"]["2"])
+    l_p3.append(item["products"]["3"])
+    l_p4.append(item["products"]["4"])
+    l_p5.append(item["products"]["5"])
+    l_p6.append(item["products"]["6"])
+    l_p7.append(item["products"]["7"])
+    l_p8.append(item["products"]["8"])
+    l_p9.append(item["products"]["9"])
+    l_p10.append(item["products"]["10"])
+
+lst1=[l_p1, l_p2, l_p3, l_p4, l_p5, l_p6, l_p7, l_p8, l_p9, l_p10]
+print(lst1)
+lst_promedio_on=[]
+for i in lst1:
+    lst_promedio_on.append(metodos_pruebas.promedio(i))
+
+
 
 
 
